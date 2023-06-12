e19633cf877cbb7aaaee455970b7ea635fc5bc55
Georgios Tasioulis <georgios.tasioulis@eurodyn.com>
2023-06-12 11:18:17 +0300

    connectDb.js and establish DB Connection

--- dataProtection.js
+++ dataProtection.js
@@ -1,7 +1,9 @@
 const crypto = require("crypto-js");
+const con = require("./connectDb");
 
 const dataEncryption = (message, secretKey) => {
   const cipherText = crypto.AES.encrypt(message, secretKey);
+
   return cipherText;
 };
 
@@ -10,5 +12,11 @@ const dataDecryption = (message, secretKey) => {
   const originalTest = bytes.toString(crypto.enc.Utf8);
   return originalTest;
 };
-
+con.query(
+  `INSERT INTO data (idData,dataName,userName,password,email,secretKey) VALUES ("1","1","1","1","1","1")`,
+  (err, rows) => {
+    if (err) throw err;
+    console.log("Data:", rows);
+  }
+);
 module.exports = { dataEncryption, dataDecryption };
